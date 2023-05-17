ee89352a7556c39398ee164bcd2f17bdf8616317
Tirth Patel <tirth1093@gmail.com>
2023-05-17 11:14:09 +0530

    setup authjs + prisma

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,28 +1,28 @@
 import { type SolidAuthConfig } from "@solid-auth/base";
 import Google from "@auth/core/providers/google";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { prisma } from "./db/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./db";
 import { serverEnv } from "~/env/server";
 
-// declare module "@auth/core/types" {
-//   export interface Session {
-//     user?: {
-//       id?: string;
-//     } & DefaultSession["user"];
-//   }
-// }
+declare module "@auth/core/types" {
+  export interface Session {
+    user?: {
+      id?: string;
+    } & DefaultSession["user"];
+  }
+}
 
 export const authOptions: SolidAuthConfig = {
-  // callbacks: {
-  //   session({ session, user }) {
-  //     if (session.user) {
-  //       session.user.id = user.id;
-  //     }
-  //     return session;
-  //   },
-  // },
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   // // @ts-expect-error - this is a valid adapter
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: serverEnv.GOOGLE_CLIENT_ID,
